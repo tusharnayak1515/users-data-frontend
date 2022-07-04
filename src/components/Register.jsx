d9a59909ca1935db764055aafcf1869e95d52569
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2022-07-04 17:54:08 +0530

    displaying error on login page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -10,6 +10,7 @@ const Register = () => {
   const dispatch = useDispatch();
   const {user} = useSelector(state=> state.userReducer,shallowEqual);
   const [userDetails, setUserDetails] = useState({name: "", email: "", phone: "", password: ""});
+  const [error, setError] = useState();
 
   const onValueChange = (e)=> {
     const { name, value } = e.target;
@@ -19,8 +20,28 @@ const Register = () => {
 
   const onRegister = (e)=> {
     e.preventDefault();
-    dispatch(actionCreators.register(userDetails));
-    setUserDetails({name: "", email: "", phone: "", password: ""});
+    if(userDetails.name.length >= 5 && 
+      userDetails.email.match("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
+     && userDetails.phone.length === 10
+     && userDetails.password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/)) 
+     {
+       dispatch(actionCreators.register(userDetails));
+       setUserDetails({name: "", email: "", phone: "", password: ""});
+     }
+     else {
+      if(userDetails.name.length < 5 ) {
+        setError("Name must be minimum 5 characters");
+      }
+      else if(!userDetails.email.match("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")) {
+        setError("Enter a valid email!");
+      }
+      else if(userDetails.phone.length !== 10) {
+        setError("Enter a valid phone!");
+      }
+      else {
+        setError("Password must contain atleast 1 uppercase, 1 lowercase, 1 special character, and 1 number!");
+      }
+     }
   }
     
   useEffect(()=> {
@@ -36,6 +57,7 @@ const Register = () => {
             <input type="email" name='email' value={userDetails.email} onChange={onValueChange} placeholder="Email Address" />
             <input type="number" name='phone' value={userDetails.phone} onChange={onValueChange} placeholder="Phone" />
             <input type="password" name='password' value={userDetails.password} onChange={onValueChange} placeholder="Password" />
+            {error && <p style={{color: "red", textAlign: "center"}}>{error}</p>}
             <button onClick={onRegister}>Register</button>
         </div>
     </div>
